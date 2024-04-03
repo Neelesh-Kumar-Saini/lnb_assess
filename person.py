f54bf8ba6a73ff4c5cf66e2f722f4ad19cc0ed9e
Neelesh-Kumar-Saini <78290043+Neelesh-Kumar-Saini@users.noreply.github.com>
2024-04-03 20:56:17 +0530

    Python-2

---
+++ person.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def __str__(self):
+        return f"Name: {self.name}, Age: {self.age}, Gender: {self.gender}"
+
+person1 = Person("John", 30, "Male")
+print(person1)
